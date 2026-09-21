fb76fc571232ca70784a3eccc37f88696fe7b7b3
Yash <yashprogrammers@gmail.com>
2026-09-22 01:57:36 +0530

    socket backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = require("./app");
 const connectDB = require("./config/db");
+
+const server = http.createServer(app);
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -17,8 +22,22 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/rooms", roomRoutes);
 
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173/'
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
 
 // Start server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
